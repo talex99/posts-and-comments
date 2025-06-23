6086eb549fa9a3ed878dcbc26b06172db50eb971
Ana Tanaskovic <t.anaskovic0705@gmail.com>
2025-06-23 13:48:24 +0200

    tools initialized

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+// eslint.config.js
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    plugins: ["prettier"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      // Pravila Prettier-a se automatski uključuju iz plugin-a,
+      // možeš dodati ili prepraviti ako želiš npr:
+      // "prettier/prettier": ["error"]
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  },
+);
